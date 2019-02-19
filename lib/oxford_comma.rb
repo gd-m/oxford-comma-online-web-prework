9797fdcb793f5b6f13bd4c72a5d0644499328efc
Gitanjali Mule <gitanjalidmule@gmail.com>
2019-02-19 23:23:13 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -4,7 +4,7 @@ def oxford_comma(array)
   elsif array.length == 2
     array.join(" and ")
   elsif array.length >= 3
-    new_array = array.slice(0, array.length - 2)
+    new_array = array.slice(0, array.length - 1)
     return "#{new_array.join(", ")} and #{array[array.length - 1]}"
   end
 end
